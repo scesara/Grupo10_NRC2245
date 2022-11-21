1574bf412df15fa4b4ec6860dd1866347ec941fe
santiagobd <113074703+santiagobd@users.noreply.github.com>
2022-11-20 23:00:11 -0500

    Add files via upload

---
+++ formulario/src/App.jsx
@@ -0,0 +1,20 @@
+
+import reactLogo from './assets/instaya.svg'
+import './App.css'
+import Nuevaorden from './Nuevaorden'
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <a href="">
+        <img src= {reactLogo} className="logoinstaya" />
+      </a>
+      <Nuevaorden/>
+    </div>
+  )
+}
+
+export default App
